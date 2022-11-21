428d5d6d0929769e848886528e5a73991026fa00
dali14 <dalijlasii@gmail.com>
2022-11-21 23:40:00 +0100

    front login page

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
 <html>
     <head>
+
         <title>ChatBox</title>
+        <link rel ="stylesheet" href="css/style.css">
+
     </head>
+    <body>
+        <div id="main">
+            <br><br><br>
+            <h4> Manager your teams easly and secretly </h4>
+        <img src='icon.png' width="100px"/><br>
+        <table>
+            <from methode="post" action="#">
+             <input type="text" name="username" placeholder="username"><br>
+             <input type="password" name="password" placeholder="password"><br>
+             <button type="submit" name="login">Login</button>
+            </form>
+        </table>
+        </div>
+    <div id="footer">
+       all right reserved 2022
+</div>
+
+    </body>
 </html>
