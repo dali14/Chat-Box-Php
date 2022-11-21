deef23b2ff5150e4082a18721cd7b9749a3d85ff
dali14 <dalijlasii@gmail.com>
2022-11-22 00:07:05 +0100

    home page front

---
+++ home.php
@@ -0,0 +1,30 @@
+
+<html>
+    <head>
+
+        <title>home</title>
+        <link rel ="stylesheet" href="css/style.css">
+
+    </head>
+    <body>
+        <div id="main_home">
+            <div id="userinfo">
+                userinfo 
+            </div>
+                <div id="msgscreen">
+                </div>
+            <div id="msgbox">
+                <form methode="post" action="#">
+                    <textarea name="messeage" cols="10" rows="4"></textarea>
+                    <button type="submit">Send</button>
+                </form>
+            </div>
+        </div> 
+    
+
+    <div id="footer">
+       all right reserved 2022
+</div>
+
+    </body>
+</html>
--- index.php
+++ index.php
@@ -11,7 +11,7 @@
             <h4> Manager your teams easly and secretly </h4>
         <img src='icon.png' width="100px"/><br>
         <table>
-            <from methode="post" action="#">
+            <from methode="post" action="home.php">
              <input type="text" name="username" placeholder="username"><br>
              <input type="password" name="password" placeholder="password"><br>
              <button type="submit" name="login">Login</button>
